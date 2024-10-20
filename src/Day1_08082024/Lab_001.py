57083cd0186f571364a012e50255a2f4fd0cd882
Nanda <akshithnandakommu@gmail.com>
2024-10-20 20:24:35 +0530

    Day1

---
+++ src/Day1_08082024/Lab_001.py
@@ -0,0 +1,17 @@
+print("Hello python")
+
+#This is a comment
+"""
+These
+are
+multiple
+line
+comments
+"""
+print("Let's have some fun with Python")
+print(2+2)
+print(2.5+9.13+0.034)
+print(-12-18)
+print(0.0023-0.0124)
+print(31/4)
+print(2*2*3)
